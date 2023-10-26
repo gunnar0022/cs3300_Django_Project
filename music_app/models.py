00c8cb7d09e878fc722404b4d8cad92b95afffb6
Gunnar Anderson <gunnar0022@gmail.com>
2023-10-26 15:11:44 -0600

    Added 'song' and 'user' models into my database. They can now be added manually with Admin

--- music_app/models.py
+++ music_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name='Name')
+    INSTRUMENT_CHOICES = [
+        # Add your choices here. Example:
+        ('guitar', 'Guitar'),
+        ('piano', 'Piano'),
+    ]
+    instrument = models.CharField(max_length=255, choices=INSTRUMENT_CHOICES, verbose_name='Instrument')
+    
+    GENRE_CHOICES = [
+        # Add your choices here. Example:
+        ('rock', 'Rock'),
+        ('jazz', 'Jazz'),
+    ]
+    genre = models.CharField(max_length=255, choices=GENRE_CHOICES, verbose_name='Genre')
+    users = models.ManyToManyField('User', related_name='songs', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Name')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    bio = models.TextField(blank=True, verbose_name='Bio')
+
+    def __str__(self):
+        return self.name
